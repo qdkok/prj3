a02cb6ab9673c49e0cf701ccfb803cfae0ab3abf
class4 <class4@DESKTOP-0ASTNHG>
2018-03-07 15:48:54 +0900

    1

---
+++ project_3/src/kr/co/sist/admin/dao/HklandAdminDAO.java
@@ -0,0 +1,77 @@
+package kr.co.sist.admin.dao;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class HklandAdminDAO {
+	private static HklandAdminDAO embd;
+	private static SqlSessionFactory ssf;
+
+	private HklandAdminDAO() {
+
+	}// ExamMyBatisDAO
+
+	public static HklandAdminDAO getInstance() {
+		if (embd == null) {
+			embd = new HklandAdminDAO();
+		} // end if
+		return embd;
+	}// getInstance
+
+	private synchronized SqlSessionFactory getSqlSessionFactory() throws IOException {
+		if (ssf == null) {
+			org.apache.ibatis.logging.LogFactory.useLog4JLogging();
+			Reader reader = null;
+			try {
+				reader = Resources.getResourceAsReader("kr/co/sist/admin/dao/sql_map_config.xml");
+				ssf = new SqlSessionFactoryBuilder().build(reader);
+			} finally {
+				if (reader != null) {
+					reader.close();
+				} // end if
+			} // end finally
+		} // end if
+		return ssf;
+	}// getSqlSessionFactory
+
+	public List<String> selectEname() throws SQLException, IOException {
+		List<String> id = null;
+		SqlSession ss = null;
+
+		try {
+			ss = getSqlSessionFactory().openSession();
+			 id = ss.selectList("kr.co.sist.exam3.selAdim");
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (ss != null) {
+				ss.close();
+			} // end if
+		} // end finally
+
+		return  id;
+	}// selectEname
+
+	public static void main(String[] args) {
+		HklandAdminDAO dmbs=HklandAdminDAO.getInstance();
+		try {
+			List<String> test=dmbs.selectEname();
+			for(String tt:test) {
+				System.out.println(tt);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}// class
